8b559cde3dafff3e2e1d76c09cefa26236d5e638
sonapap <sona.papazyan@gmail.com>
2019-06-22 13:27:27 +0400

    'lalalal'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './component/common/Header';
+import Detail from './component/detail/Detail';
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Detail />
+        </div>
+    )
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header';
-import List from './components/List';
-
-const tag = <h2>Hello</h2>;
+import App from './App';
+import './index.css';
 
 
-const App = () => {
-    return (
-        <div>
-            <Header 
-                title = 'Hello world'
-                message = 'Welcome React'
-                userName = 'Name'
-            />
-            <List />
-        </div>
-    )
-}
+
 
 ReactDOM.render(
     <div>
